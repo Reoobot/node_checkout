5c48ad746d39fd8a03034b71c4416630acb86e59
Freddy Vegas <vegasfalexis@gmail.com>
2023-07-22 21:29:03 +0200

    try

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -4,38 +4,99 @@ import {STRIPE_PRIVATE_KEY} from '../config.js'
 
 const stripe = new Stripe (STRIPE_PRIVATE_KEY);
 
+
 export const createSeccion = async (req, res)=> {
-   const session = await stripe.checkout.sessions.create({
-        line_items:[
+    try {
+        const session = await stripe.checkout.sessions.create({
+          line_items: [
             {
-                price_data:{
-                    product_data:{
-                        name:'Lapto',
-                        description:'Gaming Lapto',
-                    },
-                    currency:'usd',
-                    unit_amount: 20000,
+              price_data: {
+                product_data: {
+                  name: 'Laptop',
+                  description: 'Gaming Laptop',
                 },
-                quantity:1,
+                currency: 'usd',
+                unit_amount: 20000,
+              },
+              quantity: 1,
             },
             {
-                price_data:{
-                    product_data:{
-                        name:'TV',
-                        description:'Smart TV',
-                    },
-                    currency:'usd',
-                    unit_amount: 10000,
+              price_data: {
+                product_data: {
+                  name: 'TV',
+                  description: 'Smart TV',
                 },
-                quantity:2,
-            }   
-        ],
-        mode:'payment',
-        success_url:'http://localhost:3000/success',
-        cancel_url:'http://localhost:3000/cancel'
-    })
-    res.header("Content-Type", "application/json");
-    res.json({ url: session.url });
-};
+                currency: 'usd',
+                unit_amount: 10000,
+              },
+              quantity: 2,
+            },
+          ],
+          mode: 'payment',
+          success_url: 'http://localhost:3000/success',
+          cancel_url: 'http://localhost:3000/cancel',
+        });
+    
+        res.json({ url: session.url });
+      } catch (error) {
+        console.error('Error creating checkout session:', error.message);
+        res.status(500).json({ error: 'An error occurred while creating the checkout session' });
+      }
+    };
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const createSeccion = async (req, res)=> {
+//    const session = await stripe.checkout.sessions.create({
+//         line_items:[
+//             {
+//                 price_data:{
+//                     product_data:{
+//                         name:'Lapto',
+//                         description:'Gaming Lapto',
+//                     },
+//                     currency:'usd',
+//                     unit_amount: 20000,
+//                 },
+//                 quantity:1,
+//             },
+//             {
+//                 price_data:{
+//                     product_data:{
+//                         name:'TV',
+//                         description:'Smart TV',
+//                     },
+//                     currency:'usd',
+//                     unit_amount: 10000,
+//                 },
+//                 quantity:2,
+//             }   
+//         ],
+//         mode:'payment',
+//         success_url:'http://localhost:3000/success',
+//         cancel_url:'http://localhost:3000/cancel'
+//     })
+//     res.header("Content-Type", "application/json");
+//     res.json({ url: session.url });
+// };
 
 
--- src/routes/payment.routers.js
+++ src/routes/payment.routers.js
@@ -7,5 +7,13 @@ router.post("/checkout-session", createSeccion);
 router.get('/success',(req, res)=> res.redirect("/success.html"));
 router.get('/cancel',(req, res)=> res.redirect("/"));
 
+// router.get('/cancel', (req, res) => {
+//     res.redirect(path.resolve('path/to/cancel.html'));
+//   });
+
+
+//   router.get('/success', (req, res) => {
+//     res.redirect(path.resolve('path/to/success.html'));
+//   });
 
 export default router;
