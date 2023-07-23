2bd3a4ca3a5887f15465045183d74db1222fd202
Freddy Vegas <vegasfalexis@gmail.com>
2023-07-23 13:54:05 +0200

    env

--- src/routes/payment.routers.js
+++ src/routes/payment.routers.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { createSeccion } from "../controllers/payment.controller.js";
 
+import { API_BASE_URL } from "../config.js";
+
 
 const router = Router();
 
 
 
-router.post('/checkout-session', createSeccion);
+router.post(`${API_BASE_URL}/checkout-session`, createSeccion);
 console.log(router);
 router.get('/success',(req, res)=> res.redirect('/success.html'));
 router.get('/cancel',(req, res)=> res.redirect('/'));
